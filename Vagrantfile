3186196d30eb5b85e01635e02bf6c877165fae59
Miguel Cabral <miguel.rb.cabral@gmail.com>
2018-02-11 17:53:22 -0300

    atualização do vagrantfile
    vagrantfile e script de instalação atualizados

--- Vagrantfile
+++ Vagrantfile
@@ -5,15 +5,34 @@ Vagrant.configure(2) do |config|
 
   config.vm.define "monitorserver" do |monitorserver|
     monitorserver.vm.hostname = "monitorserver"
-    monitorserver.vm.box = "debian/stretch64"
-    monitorserver.vm.network "forwarded_port", guest: 80, host: 8080
-    config.vm.network "forwarded_port", guest: 3306, host: 3306
-    monitorserver.vm.network "private_network", ip: "192.168.0.2", virtualbox__intnet: "RedeVagrant"
+    monitorserver.vm.box = "ubuntu/xenial64"
+    monitorserver.vm.box_version = "20180126.0.0"
+    monitorserver.vm.network "forwarded_port", guest: 80, host: 80
+    monitorserver.vm.network "forwarded_port", guest: 3306, host: 3306
+    # monitorserver.vm.network "private_network", ip: "192.168.0.2", virtualbox__intnet: "RedeVagrant"
+    ## A linha seguinte configura uma rede bridge Ethernet
+    # monitorserver.vm.network "public_network", :bridge => 'Realtek PCIe Family Controller'
+    ## A linha seguinte configura uma rede bridge WiFi
+    monitorserver.vm.network "public_network", :bridge => 'Dell Wireless 1705 802.11b/g/n (2.4GHZ)'
     monitorserver.vm.synced_folder ".", "/var/www/html"
     monitorserver.vm.provider "virtualbox" do |vb|
       vb.name = "monitorserver"
     end
+    monitorserver.vm.provision "shell", inline: <<-SHELL
+    sudo su
+    useradd monitor
+    passwd -d -u monitor
+    chage -d0 monitor
+    echo "monitor:monitor" | sudo chpasswd
+    cd
+    pwd
+    echo -e "root\nroot" | passwd root
+    sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    cp -rvf /home/vagrant/.ssh/ /root/
+    true
+    SHELL
     monitorserver.vm.provision "shell", path: "./server-install.sh"
+
   end
 
 end
--- server-install.sh
+++ server-install.sh
@@ -1,40 +1,41 @@
-# echo "Changing password"
-# passwd -d -u monitor
-# chage -d0 monitor
-# echo "monitor:monitor" | sudo chpasswd
+#echo "Changing password"
+#sudo useradd monitor
+#passwd -d -u monitor
+#chage -d0 monitor
+#echo "monitor:monitor" | sudo chpasswd
 
 echo "Updating apt"
-sudo apt -y update > /dev/null
+sudo apt-get -y update > /dev/null
 
 echo "Installing Apache"
-sudo apt -y install apache2 > /dev/null
+sudo apt-get -y install apache2 > /dev/null
 
 echo "Installing PHP"
 sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php > /dev/null
-sudo apt -y update > /dev/null
-sudo LC_ALL=C.UTF-8 apt -y install php7.1 php7.1-mysql > /dev/null
+sudo apt-get -y update > /dev/null
+sudo LC_ALL=C.UTF-8 apt-get -y install php7.1 php7.1-mysql > /dev/null
 sudo service apache2 restart > /dev/null
 
 
 echo "Configurating PHP SSH2"
 # https://gist.github.com/magnetikonline/48ce1d1dca53b44666ba9332bc41c698
-sudo apt -y install autoconf libssh2-1-dev > /dev/null
+sudo apt-get -y install autoconf libssh2-1-dev > /dev/null
 sudo cp /var/www/html/ssh2.so /usr/lib/php/20160303/ssh2.so
 echo 'extension=ssh2.so' | sudo tee --append /etc/php/7.1/mods-available/ssh2.ini
 sudo ln -s /etc/php/7.1/mods-available/ssh2.ini /etc/php/7.1/cli/conf.d/20-ssh2.ini
-echo '\n\n[ssh2]\nextension=ssh2.so' | sudo tee --append /etc/php/7.1/apache2/php.ini
+echo -e "\n\n[ssh2]\nextension=ssh2.so" | sudo tee --append /etc/php/7.1/apache2/php.ini
 sudo service apache2 restart > /dev/null
 
 
-echo "Installing MariaDB"
+echo "Installing MySQL"
 DBPASSWD=abc123
 echo "mysql-server mysql-server/root_password password $DBPASSWD" | sudo debconf-set-selections  > /dev/null
 echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | sudo debconf-set-selections  > /dev/null
-sudo apt -y install mysql-server  > /dev/null
+sudo apt-get -y install mysql-server  > /dev/null
 
 
 echo "Installing JRE"
-sudo apt -y install default-jre  > /dev/null
+sudo apt-get -y install default-jre  > /dev/null
 
 echo "Vagrant finish installing"
 echo "Installing Network Monitor Server"
